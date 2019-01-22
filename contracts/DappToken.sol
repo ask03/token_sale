5018af4175d5f0075dc2a270985d9c6ffefd387e
Allan Kim <alkim03@gmail.com>
2019-01-22 21:28:59 +0900

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,15 +1,36 @@
 pragma solidity ^0.5.0;
 
 contract DappToken {
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
-    uint public totalSupply;
-    
-    constructor () public {
-      totalSupply = 1000000;
+
+    string public name = 'DApp Token';
+    string public symbol = 'DAPP';
+    string public standard = 'DApp Token v1.0';
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(
+      address indexed _from,
+      address indexed _to,
+      uint256 _value
+    );
+
+    constructor (uint256 _initialSupply) public {
+      balanceOf[msg.sender] = _initialSupply;
+      totalSupply = _initialSupply;
+      // allocate the initial supply
     }
 
+    // Transfer
+    // Exception if account doesn't have enough
+    // Return a boolean
+    // Transfer Event
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+      require(balanceOf[msg.sender] >= _value);
+      balanceOf[msg.sender] -= _value;
+      balanceOf[_to] += _value;
+      emit Transfer(msg.sender, _to, _value);
+      return true;
+    }
 
-
-}
+  }
