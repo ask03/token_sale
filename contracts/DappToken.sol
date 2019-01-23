16d61619b2f8893d18efb35a9723143b320d0f39
Allan Kim <alkim03@gmail.com>
2019-01-23 15:06:25 +0900

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,29 +7,51 @@ contract DappToken {
     string public standard = 'DApp Token v1.0';
     uint256 public totalSupply;
 
-    mapping(address => uint256) public balanceOf;
-
     event Transfer(
       address indexed _from,
       address indexed _to,
       uint256 _value
     );
 
+    event Approval(
+      address indexed _owner,
+      address indexed _spender,
+      uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
     constructor (uint256 _initialSupply) public {
       balanceOf[msg.sender] = _initialSupply;
       totalSupply = _initialSupply;
-      // allocate the initial supply
     }
 
-    // Transfer
-    // Exception if account doesn't have enough
-    // Return a boolean
-    // Transfer Event
+
     function transfer(address _to, uint256 _value) public returns (bool success) {
       require(balanceOf[msg.sender] >= _value);
       balanceOf[msg.sender] -= _value;
       balanceOf[_to] += _value;
       emit Transfer(msg.sender, _to, _value);
+      return true;
+    }
+
+    // approve
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+      // allowance
+      allowance[msg.sender][_spender] = _value;
+      // approve event
+      emit Approval(msg.sender, _spender, _value);
+      return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+      require(allowance[_from][msg.sender] >= _value);
+      require(balanceOf[_from] >= _value);
+      allowance[_from][msg.sender] -= _value;
+      balanceOf[_from] -= _value;
+      balanceOf[_to] += _value;
+      emit Transfer(_from, _to, _value);
       return true;
     }
 
